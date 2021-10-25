c6361c2b97dee5b5d097587ba33ff29df6036aa7
ketangote <ketan.gote@metamagic.in>
2021-10-25 23:38:39 +0530

    Changes: Added KTable Aggregation Example of Word Count

---
+++ ktable-demo/word-count-demo/src/main/java/com/ktable/service/WordCountBinder.java
@@ -0,0 +1,13 @@
+package com.ktable.service;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.springframework.cloud.stream.annotation.Input;
+
+public interface WordCountBinder {
+
+	@Input("word-count-input-channel")
+	public KStream<String, String>  wordCountInputStream();
+	
+	
+}
---
+++ ktable-demo/word-count-demo/src/main/java/com/ktable/service/WordCountService.java
@@ -0,0 +1,37 @@
+package com.ktable.service;
+
+import java.util.Arrays;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@SuppressWarnings("deprecation")
+@Service
+@Log4j2
+@EnableBinding(value = WordCountBinder.class)
+public class WordCountService {
+
+	@StreamListener(value = "word-count-input-channel")
+	public void process(KStream<String, String> input) {
+
+		KStream<String, String> wordCountStream = input
+	                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split(" ")));
+		 
+		wordCountStream
+			 .groupBy((k,v)->v)
+			 .count()
+			 .toStream()
+			 //.map((k,v)-> KeyValue.pair(k, v.toString()))
+			 //.to("aggregate-word-count-topic", Produced.with(Serdes.String(),Serdes.String()));
+			 .foreach((k, v) -> System.out.println("Key = " + k + " Value = " + v));
+
+	}
+
+}
