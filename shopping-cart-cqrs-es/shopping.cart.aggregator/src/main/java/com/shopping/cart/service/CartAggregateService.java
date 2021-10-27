d4cb9bf0b2831cfdb7122c028100421d0cf3cdbe
ketangote <ketan.gote@metamagic.in>
2021-10-27 11:06:36 +0530

    Changes: Publish aggregate data on new kafka topic

--- shopping-cart-cqrs-es/shopping.cart.aggregator/src/main/java/com/shopping/cart/service/CartAggregateService.java
+++ shopping-cart-cqrs-es/shopping.cart.aggregator/src/main/java/com/shopping/cart/service/CartAggregateService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.kafka.support.serializer.JsonSerde;
@@ -28,15 +29,17 @@ public class CartAggregateService {
 
 		input.foreach((k, v) -> log.info("Key={}, Value={}", k, v));
 
-		input.mapValues(CartEvent::parse)
+		KStream<String, ShoppingCart> shoppingCartKStream = input.mapValues(CartEvent::parse)
 			.map((k,v)-> KeyValue.pair(v.getCustomerId(), v))
 			.groupByKey(Grouped.with(Serdes.String(),new JsonSerde<>(CartEvent.class)))
 			.aggregate(()-> new ShoppingCart.Builder().build(), 
 					   (k,v,aggr) -> apply(aggr, v),
 					   Materialized.with(Serdes.String(), new JsonSerde<>(ShoppingCart.class)))
-			.toStream()
-			.foreach((k,v)->log.info("Aggregate Key={}, Value={}",k,v));
+			.toStream();
 		
+		shoppingCartKStream.foreach((k,v)->log.info("CustomerId={}, Aggregate={}",k,v));
+		
+		shoppingCartKStream.to("shopping-cart-aggregate", Produced.with(Serdes.String(), new JsonSerde<>(ShoppingCart.class)));
 
 	}
 
